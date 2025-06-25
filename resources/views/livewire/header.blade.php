490655676cd9794b2d586aa50bce84d46618722f
Danbo <daniel.baker1997@outlook.com>
2025-06-25 11:55:20 +0100

    Update header.blade.php

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -7,22 +7,22 @@
     <div class="container containersocial">
         <div class="social-box">
             <div class="social-icons d-flex justify-content-center">
-                <a href="#" class="tiktok" target="_blank">
+                <a href="https://www.tiktok.com/@burgermanserj" class="tiktok" target="_blank">
                     <span class="social tiktok">
                         <i class="fa-brands fa-tiktok"></i>
                     </span>
                 </a>
-                <a href="#" class="instagram" target="_blank">
+                <a href="https://www.instagram.com/Burgermanserj/" class="instagram" target="_blank">
                     <span class="social instagram">
                         <i class="fa-brands fa-instagram"></i>
                     </span>
                 </a>
-                <a href="#" class="youtube" target="_blank">
+                <a href="https://www.youtube.com/@Burgermanserj" class="youtube" target="_blank">
                     <span class="social youtube">
                         <i class="fa-brands fa-youtube"></i>
                     </span>
                 </a>
-                <a href="#" class="snapchat" target="_blank">
+                <a href="https://www.snapchat.com/add/Burgermanserj" class="snapchat" target="_blank">
                     <span class="social snapchat">
                         <i class="fa-brands fa-snapchat"></i>
                     </span>
